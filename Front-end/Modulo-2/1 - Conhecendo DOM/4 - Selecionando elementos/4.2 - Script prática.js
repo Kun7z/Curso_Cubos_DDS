4ac1bca86dfc10f8905d45a5a6ee3651e45559b6
Kun7z <ematheuskuntz@gmail.com>
2024-09-25 01:01:29 -0300

    Prosseguido com estudo de DOM

---
+++ Front-end/Modulo-2/1 - Conhecendo DOM/4 - Selecionando elementos/4.2 - Script prática.js
@@ -0,0 +1,7 @@
+//Traz a primeira ocorrência
+const elemento = document.querySelector('.subtitulo')
+
+//Traz uma coleção (array)
+const todosOsElementos = document.querySelectorAll('h2')
+
+//Pode ser selecionado como se fosse um array console.log(todosOsElementos[0]) 
